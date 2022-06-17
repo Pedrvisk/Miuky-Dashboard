5970ab3f80614bc61ee9a561f5b2c8adc9a2ed32
Pedrσvisk <45336628+Pedrvisk@users.noreply.github.com>
2022-06-17 14:25:34 -0300

    Update commands.js
    - Fixed some bug

--- src/Resources/ServerAPI/commands.js
+++ src/Resources/ServerAPI/commands.js
@@ -2,18 +2,18 @@
 // Commands: Routers
 module.exports = (express, app, router, axios, cache) => {
     router.get('/api/commands', async (req, res) => {
-        const cacheExpire = global.functions.checkCooldown(cache.getTtl('commands'));
-        if (cacheExpire.inCooldown) return res.json(cache.get('commands'));
+        const cacheExpire = global.functions.checkCooldown(cache.getTtl(`${req.lang.code}_commands`));
+        if (cacheExpire.inCooldown) return res.json(cache.get(`${req.lang.code}_commands`));
 
         try {
             await app.client.request({
                 type: 'commands',
                 data: { lang: req.lang.code || 'en' }
-            }).then((data) => cache.set('commands', data, 300));
+            }).then((data) => cache.set(`${req.lang.code}_commands`, data, 300));
 
-            return res.json(cache.get('commands'))
+            return res.json(cache.get(`${req.lang.code}_commands`));
         } catch {
-            return res.json(cache.get('commands'))
+            return res.json(cache.get(`${req.lang.code}_commands`));
         }
     });
 }
